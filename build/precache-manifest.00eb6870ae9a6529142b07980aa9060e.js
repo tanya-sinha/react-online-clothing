9788f5b7ab7fbaa96ea482e1c0af8e44cf5e7c28
Tanya Sinha <sinha.tanyacse@gmail.com>
2020-05-13 15:54:48 +0530

    project-depolyed

---
+++ build/precache-manifest.00eb6870ae9a6529142b07980aa9060e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b06ab9bd208ff21daeb77fba4fb4c9",
+    "url": "/online-clothing/index.html"
+  },
+  {
+    "revision": "7393328fe693aed142d0",
+    "url": "/online-clothing/static/css/main.7aa8b955.chunk.css"
+  },
+  {
+    "revision": "98bf4b9bcdaad8d2d74d",
+    "url": "/online-clothing/static/js/2.50150ebe.chunk.js"
+  },
+  {
+    "revision": "39d7bc06955b7296253a7d4b7064b04d",
+    "url": "/online-clothing/static/js/2.50150ebe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7393328fe693aed142d0",
+    "url": "/online-clothing/static/js/main.5aeeb61f.chunk.js"
+  },
+  {
+    "revision": "3e87ffb32ecc8e0862b2",
+    "url": "/online-clothing/static/js/runtime-main.aa07703d.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/online-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/online-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
